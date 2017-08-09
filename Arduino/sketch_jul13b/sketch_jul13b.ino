8f6a0661035575e9c507f2cd03d9538f84bfaadf
Wynonna C <wycham00@gmail.com>
2017-08-09 14:05:12 -0400

    Add files via upload

---
+++ Arduino/sketch_jul13b/sketch_jul13b.ino
@@ -0,0 +1,26 @@
+void setup() {
+  // put your set up code here, to run code: 
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  digitalWrite(13, HIGH);   
+  delay(1000);              
+  digitalWrite(13, LOW);
+  delay(1000);  
+  digitalWrite(12, HIGH);   
+  delay(1000);              
+  digitalWrite(12, LOW);
+  delay(1000);            
+}
+
+// go to File
+// Examples
+// 0.1Basics
+// Blink 
+  // new window opens, where you need to Verify, Upload 
+  // and then exit to original window --> this window
+
+// Now Upload in this window
---
+++ Arduino/sketch_jul14a/sketch_jul14a.ino
@@ -0,0 +1,28 @@
+// Robotics with the BOD Shield - ForwardThreeSeconds
+// Make the BOE Shield-Bot roll forward for three seconds, then stop.
+
+#include <Servo.h>
+
+Servo servoLeft;
+Servo servoRight;
+
+void setup() {
+  // put your setup code here, to run once:
+
+// tone(4, 3000, 1000);
+// delay(1000);
+  
+  servoLeft.attach(13);
+  servoRight.attach(12);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ // Full speed forward
+  servoLeft.writeMicroseconds(1700); 
+  servoRight.writeMicroseconds(1300); 
+  delay(3000);
+
+  servoLeft.detach();
+  servoRight.detach();
+}
